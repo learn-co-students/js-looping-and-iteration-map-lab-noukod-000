d4d5f5714e55dfa0cc0fef012c75f49edcd83e71
Gilbert Torchon <gilbert@noukod.com>
2019-02-07 18:56:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  return array.map((driver)=>driver.toLowerCase());
+}
+
+function nameToAttributes(array){
+  return array.map((name)=>Object.assign({},{firstName:name.split(" ")[0]},{lastName:name.split(" ")[1]}))
+}
+
+function attributesToPhrase(array){
+  return array.map((element)=>`${element.name} is from ${element.hometown}`)
+}
