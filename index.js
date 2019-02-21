0ca051895aaf46ffb469fb970d2a7abf2ce23a25
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-21 15:44:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(lower)
+{
+    return lower.map((driver) => driver.toLowerCase());
+}
+
+function nameToAttributes(profile_user){
+  return profile_user.map(function(user_info){
+    const user_infoFirst = user_info.split(' ')[0];
+    const user_infoLast = user_info.split(' ')[1];
+
+    return { firstName: user_infoFirst, lastName: user_infoLast };
+  });
+}
+
+function attributesToPhrase(attributes){
+  return attributes.map(function(driver){
+    return `${driver.name} is from ${driver.hometown}`
+  });
+}
