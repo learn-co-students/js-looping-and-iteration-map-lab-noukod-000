5f9707b2f8a8eff7784b372b5dcff9ad50288688
Saint-Jean Germila <germila@noukod.com>
2019-03-06 18:20:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  return drivers.map(function(driverName){return driverName.toLowerCase()});
+}
+
+function nameToAttributes(drivers)
+ {
+  return drivers.map(function(driver){
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+    return {firstName: driverFirst, lastName: driverLast};
+    })
+};
+
+function attributesToPhrase(drivers)
+ {
+  const strings = [];
+  drivers.map(function(driver) {
+    strings.push(`${driver.name} is from ${driver.hometown}`)
+  })
+  return strings;
+}
+attributesToPhrase(drivers);
