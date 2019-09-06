0d79b47e0f373747a60c3265864491bfe548bf97
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-06 15:41:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (driverName){
+    return driverName.toLowerCase();
+  });
+}
+  
+function nameToAttributes(drivers) {
+   return drivers.map(function (driverName){
+     const driverFirst = driverName.split(' ')[0];
+     const driverLast = driverName.split(' ')[1];
+    return { firstName: driverFirst, lastName: driverLast };
+   });
+  
+}  
+
+function attributesToPhrase(array) {
+  return array.map(function(driver) {
+    return  `${driver.name} is from ${driver.hometown}`;
+  });
+}
+
